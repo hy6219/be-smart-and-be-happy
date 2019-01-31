d6425f11fe366b15fcca72a746ab7133e35512b3
hy6219 <39812154+hy6219@users.noreply.github.com>
2019-01-31 15:44:18 +0900

    Create cal_program.cpp

---
+++ cal_program.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "cal.h"
+/*cal.h, cal.cpp are the files for this program. They supports basic calculation!*/
+using namespace std;
+int main() {
+	cout << add(1,2) << endl;
+	cout << substract(1, 2) << endl;
+	cout << multiply(1, 2) << endl;
+	cout << divide(1, 2) << endl;
+	return 0;
+}
