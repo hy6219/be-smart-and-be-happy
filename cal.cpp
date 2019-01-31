cfc78fa576442b642fe7292ae5ccb1e9bcc76031
hy6219 <39812154+hy6219@users.noreply.github.com>
2019-01-31 15:42:54 +0900

    Create cal.cpp

---
+++ cal.cpp
@@ -0,0 +1,12 @@
+int add(int a, int b) {
+	return a + b;
+}
+int substract(int a, int b) {
+	return a - b;
+}
+int multiply(int a, int b) {
+	return a * b;
+}
+double divide(int a, int b) {
+	return (double)a /(double)b;
+}
